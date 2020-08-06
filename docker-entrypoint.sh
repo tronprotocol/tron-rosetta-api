5e90f92ca906bdfa061a8c8c298b1dcfec847509
ray.wu <wgq.quan@gmail.com>
2020-08-06 13:53:08 +0800

    add shasta conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ shopt -s nullglob
 if [[ "private" == $NET_TYPE ]]; then
   java -jar $JARNAME -d chain -c conf/privatenet.conf -w
 elif [[ "testnet" == $NET_TYPE ]]; then
-  java -jar $JARNAME -d chain -c conf/testnet.conf
+  java -jar $JARNAME -d chain -c conf/shasta.conf
 elif [[ "mainnet" == $NET_TYPE ]]; then
   java -jar $JARNAME -d chain -c conf/mainnet.conf
 else
